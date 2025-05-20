04b302b53ef67c2144f1b6480726ed793497f2c3
hcn1z1 <mohamedhamma2003@gmail.com>
2025-05-20 01:50:55 +0100

    New MODEL

--- src/train_3d.py
+++ src/train_3d.py
@@ -45,7 +45,7 @@ def train(num_epochs, batch_size, lr):
 
     model = C3DImproved(num_classes=18).to(device)
     criterion = nn.CrossEntropyLoss(weight=weights)
-    optimizer = optim.Adam(model.parameters(), lr=lr, weight_decay=1e-4)
+    optimizer = optim.SGD(model.parameters(), lr=lr, weight_decay=1e-4, momentum=0.9)
     scheduler = ReduceLROnPlateau(optimizer, mode='min', factor=0.5, patience=4)
     early_stopping = EarlyStopping(patience=7, min_delta=0.001, mode='min')
     scaler = GradScaler()  # For mixed precision training
